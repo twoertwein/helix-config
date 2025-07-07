264cc5845f91e9db3dff2b46ebcab50824c1ce6f
Torsten Wörtwein <twoertwein@gmail.com>
2025-07-07 11:47:54 -0400

    jump to type definition

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ Mostly using builtins (+ruff and ty).
 
 - `:e <path>` to open a file (supports tab completion)
 - `:b <buffer>` switch to an already opened buffer (supports tab completion)
+- `:bd` close current buffer
 
 ## Completion
 
 - `Ctrl+x+n` keyword completion
-- `Ctrl+space` LSP completion
+- `Ctrl+space` LSP completion (ruff/ty)
 - `Ctrl+x+f` path completion
 - `Ctrl+x+s` spell checking completion
+- `Ctrl+t` Go to type definition (ty)
--- lsp/ty.lua
+++ lsp/ty.lua
@@ -5,9 +5,14 @@ return {
   on_attach = function(client, bufnr)
     -- show type annotations
     vim.lsp.inlay_hint.enable(true, {  bufnr })
-    -- automatically trigger completions (on every key press - might be slow!)
-    local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+    -- extend keys that trigger auto-trigger
+    local chars = { " ", "(", ",", "." };
+    for i = 65, 90 do table.insert(chars, string.char(i)) end  -- A-Z
+    for i = 97, 122 do table.insert(chars, string.char(i)) end  -- a-z
     client.server_capabilities.completionProvider.triggerCharacters = chars
+    -- automatically trigger completions
     vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+    -- ctrl+t to jump to the type definition
+    vim.keymap.set('n', '<c-t>', vim.lsp.buf.type_definition, { buffer = bufnr })
   end,
 }
